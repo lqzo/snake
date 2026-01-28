061f009b865b8177d4314c70b13868932c34b3ea
李轻舟 <18903968616@163.com>
2026-01-28 10:53:10 +0800

    Update page title and add i18n support for title

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 import { Home } from './pages/Home';
 import { Game } from './pages/Game';
 import { GameOver } from './pages/GameOver';
 
 function App() {
+  const { t } = useTranslation();
+
+  useEffect(() => {
+    document.title = t('app.title');
+  }, [t]);
+
   return (
     <Routes>
       <Route path="/" element={<Home />} />
